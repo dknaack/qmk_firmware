a63e01aeea5096439e113de44398f2a84b255c4d
Daniel Knaack <daniel.knaack@protonmail.com>
2024-10-23 17:59:08 +0200

    add colemak-dh layer

--- keyboards/handwired/dactyl_manuform/6x6/keymaps/dknaack/keymap.c
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/dknaack/keymap.c
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_GRV ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,       KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_NUBS,
          KC_BSLS,GUI_A  ,ALT_S  ,SFT_D  ,CTL_F  ,KC_G   ,       KC_H   ,CTL_J  ,SFT_K  ,ALT_L  ,GUI_QT ,KC_EQL ,
          KC_LBRC,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,       KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_MINS,
-         KC_RBRC,KC_NO  ,TG(4)  ,TG(5)  ,KC_NO  ,KC_NO  ,       KC_NO  ,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,KC_EQL ,
+         KC_RBRC,TG(6)  ,TG(4)  ,TG(5)  ,KC_NO  ,KC_NO  ,       KC_NO  ,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,KC_EQL ,
                                          KC_ESC ,KC_LALT,       KC_DEL ,KC_TAB ,
                                                  KC_LGUI,       KC_PGUP,
                                  LT1_SPC,LT2_TAB,KC_LCTL,       KC_PGDN,LT3_ENT, KC_BSPC
@@ -139,6 +139,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_NO  ,_______,_______,_______,_______,_______,       _______,_______,_______,_______,_______,KC_NO  ,
          KC_NO  ,_______,_______,_______,_______,_______,       _______,_______,_______,_______,_______,KC_NO  ,
          KC_NO  ,_______,_______,_______,_______,_______,       _______,_______,_______,_______,_______,KC_NO  ,
+         KC_NO  ,_______,_______,_______,_______,_______,       _______,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,
+                                         KC_NO  ,KC_NO  ,       KC_NO  ,KC_NO  ,
+                                                 KC_NO  ,       KC_NO  ,
+                                 _______,_______,KC_NO  ,       KC_NO  ,_______,_______
+    ),
+
+    /* colemak-dh layer */
+    [6] = LAYOUT_dknaack(
+         KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,       KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,
+         KC_NO  ,KC_Q   ,KC_W   ,KC_F   ,KC_P   ,KC_B   ,       KC_J   ,KC_L   ,KC_U   ,KC_Y   ,KC_SCLN,KC_NO  ,
+         KC_NO  ,HOME_A ,HOME_R ,HOME_S ,HOME_T ,KC_G   ,       KC_M   ,HOME_N ,HOME_E ,HOME_I ,HOME_O ,KC_NO  ,
+         KC_NO  ,KC_Z   ,KC_X   ,KC_C   ,KC_D   ,KC_V   ,       KC_K   ,KC_H   ,KC_COMM,KC_DOT ,KC_SLSH,KC_NO  ,
          KC_NO  ,_______,_______,_______,_______,_______,       _______,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,
                                          KC_NO  ,KC_NO  ,       KC_NO  ,KC_NO  ,
                                                  KC_NO  ,       KC_NO  ,
